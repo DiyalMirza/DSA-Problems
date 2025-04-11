b657e06789a4d957a53599ad1577e3461c54ce36
Diyal95 <diyalmirza_eight@hotmail.com>
2025-04-12 03:03:04 +0500

    Time: 0 ms (100.00%) | Memory: 13.5 MB (32.20%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -3,23 +3,23 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         // Set up a DP array
         vector<int> dp(days.size() + 1, 0);
-
-        // Iterate through each day in the 'days' array
+        
+        // Iterate through each day
         for (int i = 1; i <= days.size(); i++) {
             // Current day in the 'days' array
             int currentDay = days[i - 1];
 
-            // Case 1: 1-day ticket, cost from the previous day
+            // Case 1:
             dp[i] = dp[i - 1] + costs[0];
 
-            // Case 2: 7-day ticket, look for the day 7 days before the current day
+            // Case 2:
             int last7Day = i - 1;
             while (last7Day >= 0 && days[last7Day] > currentDay - 7) {
                 last7Day--;
             }
             dp[i] = min(dp[i], dp[last7Day + 1] + costs[1]);
 
-            // Case 3: 30-day ticket, look for the day 30 days before the current day
+            // Case 3:
             int last30Day = i - 1;
             while (last30Day >= 0 && days[last30Day] > currentDay - 30) {
                 last30Day--;
@@ -27,7 +27,7 @@ public:
             dp[i] = min(dp[i], dp[last30Day + 1] + costs[2]);
         }
 
-        // Return the result which is the minimum cost for traveling all days
+        // Return the result 
         return dp[days.size()];
     }
 };
